f212de947fa70da95bfa4b48f96c6daea9f776e3
Lassi Hämäläinen <Latexi95@gmail.com>
2011-09-18 14:47:29 +0300

    OCGui unohtu

---
+++ Game/ocgui.cpp
@@ -0,0 +1,98 @@
+#include "ocgui.h"
+#include <QMouseEvent>
+
+OCGui::OCGui(QObject *parent)
+    :QObject(parent)
+{
+}
+
+
+void OCGui::add(OCGuiObject *object)
+{
+    object->setParent(this);
+    mObjects.append(object);
+}
+
+void OCGui::mouseMoveEvent(QMouseEvent *event)
+{
+    for (QList<OCGuiObject*>::iterator i = mObjects.begin();i != mObjects.end();i++)
+    {
+        OCGuiObject *object = *i;
+        if (object->rect().contains(event->pos())) //Jos hiiri objectin päällä
+        {
+            if ((event->buttons() != Qt::NoButton) || object->mouseTracking())
+            {
+                object->mouseMoveEvent(event);
+            }
+        }
+    }
+}
+
+void OCGui::mousePressEvent(QMouseEvent *event)
+{
+    for (QList<OCGuiObject*>::iterator i = mObjects.begin();i != mObjects.end();i++)
+    {
+        OCGuiObject *object = *i;
+        if (object->rect().contains(event->pos())) //Jos hiiri objectin päällä
+        {
+            object->mousePressEvent(event);
+        }
+    }
+}
+
+void OCGui::mouseReleaseEvent(QMouseEvent *event)
+{
+    for (QList<OCGuiObject*>::iterator i = mObjects.begin();i != mObjects.end();i++)
+    {
+        OCGuiObject *object = *i;
+        if (object->rect().contains(event->pos())) //Jos hiiri objectin päällä
+        {
+            object->mouseReleaseEvent(event);
+        }
+    }
+}
+
+
+void OCGui::paintEvent(QPainter *p)
+{
+    for (QList<OCGuiObject*>::iterator i = mObjects.begin();i != mObjects.end();i++)
+    {
+        (*i)->paint(p);
+    }
+}
+
+void OCGui::keyPressEvent(QKeyEvent *event)
+{
+    for (QList<OCGuiObject*>::iterator i = mObjects.begin();i != mObjects.end();i++)
+    {
+        (*i)->keyPressEvent(event);
+    }
+}
+
+void OCGui::keyReleaseEvent(QKeyEvent *event)
+{
+    for (QList<OCGuiObject*>::iterator i = mObjects.begin();i != mObjects.end();i++)
+    {
+        (*i)->keyReleaseEvent(event);
+    }
+}
+
+void OCGui::mouseWheelEvent(QWheelEvent *event)
+{
+    for (QList<OCGuiObject*>::iterator i = mObjects.begin();i != mObjects.end();i++)
+    {
+        OCGuiObject *object = *i;
+        if (object->rect().contains(event->pos())) //Jos hiiri objectin päällä
+        {
+            object->mouseWheelEvent(event);
+        }
+    }
+}
+
+void OCGui::update()
+{
+    for (QList<OCGuiObject*>::iterator i = mObjects.begin();i != mObjects.end();i++)
+    {
+        (*i)->update();
+    }
+}
---
+++ Game/ocgui.h
@@ -0,0 +1,27 @@
+#ifndef OCGUI_H
+#define OCGUI_H
+#include "ocguiobject.h"
+#include <QList>
+class QPainter;
+class OCGui:public QObject
+{
+public:
+    OCGui(QObject *parent = 0);
+    void add(OCGuiObject *object);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseWheelEvent(QWheelEvent *event);
+    void keyPressEvent(QKeyEvent *event);
+    void keyReleaseEvent(QKeyEvent *event);
+    void update();
+    void paintEvent(QPainter *p);
+public slots:
+    void repaint();
+signals:
+    void updateWindow();
+private:
+    QList<OCGuiObject*> mObjects;
+};
+
+#endif // OCGUI_H
